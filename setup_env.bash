4e5fe5cec90972c35bdbf9063cb3d322db1d4c6b
Eshan <eshan.savla@gmail.com>
2023-11-25 18:45:21 +0100

    added script to create venv

---
+++ setup_env.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+venv_dir="$(pwd -P)/.venv/"
+if test -d $venv_dir;
+then
+    echo "activating virtual environment"
+    source .venv/bin/activate
+else
+    echo "creating virtual environment"
+    python3 -m venv .venv
+    echo "activating virtual environment"
+    source ./.venv/bin/activate
+    echo "$(which python3)"
+    touch .venv/COLCON_IGNORE
+    echo "activated venv"
+    python3 -m pip install --upgrade pip
+    python3 -m pip install -r requirements.txt
+fi
