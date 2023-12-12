f42427eb321d59eb1cc60293b410a7655ed90634
Eshan <eshan.savla@gmail.com>
2023-12-12 09:41:26 +0100

    started superpixel implementation

--- include/Colorizer.h
+++ include/Colorizer.h
@@ -3,17 +3,21 @@
 
 #include <stdio.h>
 #include <string>
-#include <opencv4/opencv2/opencv.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/ximgproc/slic.hpp>
 
 
 class Colorizer
 {
 private:
     cv::Mat reference_img;
+    std::string superpixel_algo;
+    cv::ximgproc::SuperpixelSLIC createSuperPixels(cv::Mat input_img, uint region_size = 10, float ruler = 10.0f);
 
 public:
     Colorizer(/* args */);
-    
+    void setReferenceImg(const cv::Mat &reference_img);
+    int colorizeGreyScale(const cv::Mat &input_img, cv::Mat &output_img);
     ~Colorizer();
 };
 
