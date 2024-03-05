4108f4ef378b19e2e33da0121e8fd49fc7a2e132
Eshan <eshan.savla@gmail.com>
2024-03-05 13:33:36 +0100

    added script to convert txt to npy

---
+++ e2r_frame_generation/txt_to_npy.py
@@ -0,0 +1,22 @@
+import numpy as np
+import os
+
+def main(filepath:str):
+    with open(filepath) as f:
+        filename = filepath.split("/")[-1].split(".")[0]
+        assert(filename != "", "Filename is empty")
+        file_data = np.loadtxt(f, dtype=np.string_)
+        print(file_data.shape)
+        print(file_data[202])
+        print(file_data[-1])
+        n = 1
+        folder_path = "/home/eshan/Downloads/e_data/"
+        assert(file_data.shape[0]%n == 0,"File is not evenly divisible by n")
+        chunks = np.split(file_data, n)
+        for i in range(len(chunks)):
+            np.save(os.path.join(folder_path, filename + "_" + str(i) + ".npy"), chunks[i])
+
+
+if __name__ == "__main__":
+    main("/home/eshan/Downloads/e_data/images.txt")
+    print("Done!")
