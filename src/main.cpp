988948e1df0c2aa755c49f821e38830ee340fa4a
Eshan <eshan.savla@gmail.com>
2024-03-23 21:26:11 +0100

    removed unused lines

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,11 @@
 using namespace colorizer;
 int main(int argc, char* argv[]){
 
-     cv::Mat reference_img = cv::imread("/home/eshan/Hochschule/FE_Projekt_Sem1/ros_ws/src/e2r_frame_generation/data/frames_0000000001.png");
-     cv::cvtColor(reference_img, reference_img, cv::COLOR_BGRA2BGR);
-// //    cv::imshow("Reference Image", reference_img);
-// //    cv::waitKey(0);
-     Colorizer colorizer = Colorizer(reference_img);
-    cv::Mat input_img = cv::imread("/home/eshan/Hochschule/FE_Projekt_Sem1/ros_ws/src/e2r_frame_generation/data/frames_0000000020.png", cv::IMREAD_GRAYSCALE);
-//    cv::imshow("Input Image", input_img);
-//    cv::waitKey(0);
-    // cv::cvtColor(input_img, input_img, cv::COLOR_GRAY2BGRA);
-    // cv::imwrite("/home/eshan/Hochschule/FE_Projekt_Sem1/ros_ws/src/e2r_frame_generation/data/frames_0000000020_gray.png", input_img);
-     cv::Mat output_img;
-     colorizer.colorizeGreyScale(input_img, output_img);
-     cv::imwrite("/home/eshan/Hochschule/FE_Projekt_Sem1/ros_ws/src/e2r_frame_generation/data/frames_0000000020_colorized.png", output_img);
-    // cv::imshow("Colorized Image", output_img);
-    // cv::waitKey(0);
+    cv::Mat reference_img = cv::imread("../data/frames_00000000018.png");
+    cv::cvtColor(reference_img, reference_img, cv::COLOR_BGRA2BGR);
+    Colorizer colorizer = Colorizer(reference_img);
+    cv::Mat input_img = cv::imread("../data/frames_0000000028.png", cv::IMREAD_GRAYSCALE);
+    cv::Mat output_img;
+    colorizer.colorizeGreyScale(input_img, output_img);
+    cv::imwrite("../data/frames_0000000028_colorized.png", output_img);
 }
