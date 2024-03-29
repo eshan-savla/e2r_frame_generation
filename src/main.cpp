f0e3edbe5f540bfcf6a6e6dc64faa376345d7789
Eshan Savla <86021023+eshan-savla@users.noreply.github.com>
2024-03-29 22:26:13 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,10 @@
 using namespace colorizer;
 int main(int argc, char* argv[]){
 
-    cv::Mat reference_img = cv::imread("../data/frames_00000000018.png");
+    cv::Mat reference_img = cv::imread("../data/frames_00000000028.png");
     cv::cvtColor(reference_img, reference_img, cv::COLOR_BGRA2BGR);
     Colorizer colorizer = Colorizer(reference_img);
-    cv::Mat input_img = cv::imread("../data/frames_0000000028.png", cv::IMREAD_GRAYSCALE);
+    cv::Mat input_img = cv::imread("../data/frames_0000000018.png", cv::IMREAD_GRAYSCALE);
     cv::Mat output_img;
     colorizer.colorizeGreyScale(input_img, output_img);
     cv::imwrite("../data/frames_0000000028_colorized.png", output_img);
